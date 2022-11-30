4eb2ccc3a2ba660d0cee9e214c9dc922d3218437
kyungmin <antcjswo@kakao.com>
2022-11-30 20:01:45 +0900

    2022.10 웹개발_Spring

---
+++ src/main/java/com/study/servlet/RequestPrams1.java
@@ -0,0 +1,28 @@
+package com.study.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/req/1")
+public class RequestPrams1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(request.getContentType());
+		System.out.println(request.getLocalPort());
+		System.out.println(request.getMethod());
+		System.out.println(request.getRequestURI());
+		System.out.println(request.getQueryString());
+				
+		
+		System.out.println();
+		
+	}
+}
